9dbe2ba7fe5cf816caabce549a1757e847d075cb
Jakob Wissö <jakob.wisso@elev.ga.lbs.se>
2019-01-16 11:10:41 +0100

    Snart Klar

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ClickScript : MonoBehaviour
+{
+    public static float score = 0;
+    public Text scoreText;
+    public AudioSource gnome;
+
+
+    public void FixedUpdate()
+    {
+        scoreText.text = score.ToString();
+
+        print("Score" + score);
+    }
+
+    public void GnomeClick()
+    {
+        score++;
+
+        gnome.Play();
+
+        scoreText.text = score.ToString();
+    }
+
+}
